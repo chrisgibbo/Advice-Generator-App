d1f907ececa002acb737fe5bcb7188f15c8d24fe
Chris Gibbons <christ487@icloud.com>
2023-05-18 19:25:56 +0100

    complete JS api function, added alert for error

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const adviceNum = document.getElementById("advice-num");
 const adviceQuote = document.getElementById("advice-quote");
+const adviceBtn = document.getElementById("advice-btn")
 
-
-
-const apiRequest = function () {
-    fetch("https://api.adviceslip.com/advice")
-      .then((res) => res.json())
-      .then((data) => {
-        adviceNum.innerHTML = "addice" + " " + '#' + " " + data.slip.id;
-        adviceQuote.innerHTML = '"' + data.slip.advice + '"';
-        
-      });
-      
-  };
-  
-  apiRequest();
+adviceBtn.addEventListener("click", function (){
+        fetch("https://api.adviceslip.com/advice")
+          .then((res) => res.json())
+          .then((data) => {
+            adviceNum.innerHTML = "addice" + " " + '#' + " " + data.slip.id;
+            adviceQuote.innerHTML = '"' + data.slip.advice + '"';
+        })
+        .catch((err) => {
+            alert("There was an error!!!!!!!", err);
+        })
+      })
